541504e2637b37b5f800dbaccdfb80957d3b9141
Sanghvi <sanghvir@amazon.com>
2017-03-13 14:16:34 -0700

    Chapter 2 GeoQuiz

---
+++ app/src/main/java/com/example/sanghvir/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.example.sanghvir.geoquiz;
+
+/**
+ * Created by sanghvir on 3/13/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+}
